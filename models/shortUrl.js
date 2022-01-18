96c0b943acb3899d2c8edcd1e07cf8b3d18eaea4
Babal <babalibrahim34@gmail.com>
2022-01-18 13:32:23 +0300

    first commit

---
+++ models/shortUrl.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const shortId = require('shortid');
+
+
+const shortUrlSchema = new mongoose.Schema({
+    full: {
+        type: 'string',
+        required: true
+    },
+    short: {
+        type: 'string',
+        required: true,
+        default: shortId.generate
+    },
+    clicks: {
+        type: 'number',
+        required: true,
+        default: 0
+    }
+})
+
+module.exports = mongoose.model('ShortUrl', shortUrlSchema);
